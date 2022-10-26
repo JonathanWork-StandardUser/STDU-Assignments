b735ca1dfd5843b27195e817f6d5a17c1294f404
Erik <edo@standarduserllc.com>
2022-10-26 10:32:11 -0500

    cha

--- Individual/Assignment 1/Factorize.py
+++ Individual/Assignment 1/Factorize.py
@@ -4,16 +4,3 @@ import math
 num = int(input("Enter a number: "))
 
 print(math.factorial(num))
-
-#factorial = 1
-#num = int(input("Enter a number: "))
-
-# If else statement to check for negative, zero, or positive number
-#if num < 0:
-#    print("Sorry, factorial does not allow negative numbers.")
-#elif num == 0:
-#    print("The factorial is 0 is 1.")
-#else:
-#    for i in range(1, num + 1):
-#        factorial = num * 1
-#    print("The factorial of", num,"is", factorial)

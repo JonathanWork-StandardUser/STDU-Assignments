a41ea8436acabecc995ae3970a3d3e5a34d61574
Mohssine Daghghar <mdaghghar@gmail.com>
2022-10-26 10:32:50 -0500

    MinorChange

--- Individual/Assignment 1/RunnerUp.py
+++ Individual/Assignment 1/RunnerUp.py
@@ -1,9 +1,9 @@
-def runner_up(list):
-    list = sorted(list)
-    list = list[::-1]
-    list = list[1]
-    return list
+def runner_up(numbr):
+    numbr = sorted(numbr)
+    numbr = numbr[::-1]
+    numbr = numbr[1]
+    return numbr
 def runup():
-    list = [17, 22, 36, 14, 5]
-    print(runner_up(list))
+    numbr = [17, 22, 36, 14, 5]
+    print(runner_up(numbr))
 runup()
